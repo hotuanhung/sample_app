84441ce69c285e092c1a5717961e954343f965f2
hotuanhung <hungchv16@gmail.com>
2023-08-22 15:30:55 +0700

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'users/new'
-<<<<<<< HEAD
+  get "users/new"
   get "static_pages/home"
   get "static_pages/help"
-=======
-=======
->>>>>>> 9d35123 (Chapter 3_4_5 Add gem config Add i18n)
-  resources :products
-  get 'demo_partials/new'
-  get 'demo_partials/edit'
-  get 'foo/bar'
-  get 'foo/baz'
-  get 'static_pages/home'
-  get 'static_pages/help'
-<<<<<<< HEAD
->>>>>>> e059cdf (Add user model)
-=======
->>>>>>> 9d35123 (Chapter 3_4_5 Add gem config Add i18n)
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
