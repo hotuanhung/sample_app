4f37557e84d8f8f71b9fd0096a0fd2ffc6e5b936
hotuanhung <hungchv16@gmail.com>
2023-08-30 09:59:02 +0700

    done14alll

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,34 @@
+class RelationshipsController < ApplicationController
+  before_action :logged_in_user
+  before_action :load_user, only: :create
+  before_action :load_relationship, only: :destroy
+
+  def create
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html{redirect_to @user}
+      format.js
+    end
+  end
+
+  def destroy
+    @user = @relationship.followed
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html{redirect_to @user}
+      format.js
+    end
+  end
+
+  private
+
+  def load_user
+    @user = User.find_by id: params[:followed_id]
+    redirect_to root_path, flash: {warning: t("rela.not_found")} if @user.nil?
+  end
+
+  def load_relationship
+    @relationship = Relationship.find_by id: params[:id]
+    redirect_to root_path, flash: {warning: t("rela.not_found")} if @user.nil?
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ApplicationRecord
   before_create :create_activation_digest
 
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: Relationship.name,
+  foreign_key:  "follower_id",
+  dependent: :destroy
+  has_many :passive_relationships, class_name: Relationship.name,
+  foreign_key:  "followed_id",
+  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   validates :name, presence: true,
             length: {maximum: Settings.validate.name.length.max}
@@ -80,7 +88,22 @@ class User < ApplicationRecord
   end
 
   def feed
-    microposts
+    Micropost.relate_post(following_ids << id)
+  end
+
+  # Follows a user.
+  def follow other_user
+    following << other_user unless self == other_user
+  end
+
+  # Unfollows a user.
+  def unfollow other_user
+    following.delete other_user
+  end
+
+  # Returns true if the current user is following the other user.
+  def following? other_user
+    following.include? other_user
   end
 
   private
